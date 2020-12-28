73d0f35e43e6beabeb6c32378dadfddf6ff8f986
moroball14 <daiki.mr.0330@gmail.com>
2020-12-28 18:02:37 +0900

    change method for mac

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require("gulp");
 
 gulp.task("default", function(done){
-  console.log("hello world");
+  console.log(process.env.NODE_ENV);
   done(); // 参考：https://qiita.com/tatsuo-iriyama/items/08ba4bd621b7fdedcc4e
 });
