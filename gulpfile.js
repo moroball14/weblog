080c74d06ef4ae005db960a5dc00b65b79e195de
moroball14 <daiki.mr.0330@gmail.com>
2020-12-28 18:18:34 +0900

    fix variable name

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require("gulp");
+var requireDir = require("require-dir"); // 参考：https://www.npmjs.com/package/require-dir
+
+requireDir("./gulp/tasks", { recurse: true });
 
 gulp.task("default", function(done){
   console.log(process.env.NODE_ENV);
