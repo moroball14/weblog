818749e4ea5d5cf14f679da68fb893cb3a798dc3
moroball14 <daiki.mr.0330@gmail.com>
2020-12-31 15:15:47 +0900

    correspond dev and prod

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
+var logger = require("./lib/log/logger.js").console;
 
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use("/", require("./routes/index.js"));
 
