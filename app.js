9f46309b3ae6e0956fc7f70019983f567beac145
moroball14 <daiki.mr.0330@gmail.com>
2021-01-01 11:48:35 +0900

    function input posts is added

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var accesslogger = require("./lib/log/accesslogger.js");
 var systemlogger = require("./lib/log/systemlogger.js");
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
-app.use("/public", express.static(__dirname + "/public" + (process.env.NODE_ENV === "development" ? "development" : "production")));
+app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use(accesslogger());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/", require("./routes/index.js"));
 app.use("/posts/", require("./routes/posts.js"));
