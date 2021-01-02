e7faca9784c5a49ece2ecf8c2ff0ea334a156084
moroball14 <daiki.mr.0330@gmail.com>
2021-01-02 09:06:55 +0900

    function post confirm is added

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,26 @@
 var router = require("express").Router();
 
+var validateRegistData = function (body) {
+  var isValidated = true, errors = {};
+
+  if (!body.url) {
+    isValidated = false;
+    errors.url = "URLが未入力です。'/'から始まるURLを入力してください。";
+  }
+
+  if (body.url && /^\//.test(body.url) === false) {
+    isValidated = false;
+    errors.url = "'/'から始まるURLを入力してください。";
+  }
+
+  if (!body.title) {
+    isValidated = false;
+    errors.title = "タイトルが未入力です。任意のタイトルを入力してください。";
+  }
+
+  return isValidated ? undefined : errors;
+};
+
 var createRegistData = function (body) {
   var datetime = new Date();
   return {
@@ -24,6 +45,16 @@ router.get("/posts/regist", (req, res) => {
 router.post("/posts/regist/input", (req, res) => {
   var original = createRegistData(req.body);
   res.render("./account/posts/regist-form.ejs", { original });
+});
+
+router.post("/posts/regist/confirm", (req, res) => {
+  var original = createRegistData(req.body);
+  var errors = validateRegistData(req.body);
+  if (errors) {
+    res.render("./account/posts/regist-form.ejs", { errors, original });
+    return;
+  }
+  res.render("./account/posts/regist-confirm.ejs", { original });
 });
 
 module.exports = router;
