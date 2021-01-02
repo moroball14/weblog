5aeaed37877193ab4f03ab6c498cf678463debca
moroball14 <daiki.mr.0330@gmail.com>
2021-01-02 13:15:38 +0900

    function logout is added

--- routes/account.js
+++ routes/account.js
@@ -48,6 +48,11 @@ router.get("/login", (req, res) => {
 
 router.post("/login", authenticate());
 
+router.post("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/account/login");
+});
+
 router.get("/posts/regist", authorize("readWrite"), (req, res) => {
   tokens.secret((error, secret) => {
     var token = tokens.create(secret);
