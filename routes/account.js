ac7e248e00248ceb53e215eab81ee24877fa6c90
moroball14 <daiki.mr.0330@gmail.com>
2021-01-02 13:04:54 +0900

    function authorize is added

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,5 @@
 var { CONNECTION_URL, OPTIONS, DATABSE } = require("../config/mongodb.config");
-var { authenticate } = require("../lib/security/accountcontrol.js");
+var { authenticate, authorize } = require("../lib/security/accountcontrol.js");
 var router = require("express").Router();
 var MongoClient = require("mongodb").MongoClient;
 var tokens = new require("csrf")();
@@ -38,13 +38,7 @@ var createRegistData = function (body) {
   };
 };
 
-router.get("/", (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.redirect("/account/login");
-  }
-}, (req, res) => {
+router.get("/", authorize("readWrite"), (req, res) => {
   res.render("./account/index.ejs");
 });
 
@@ -54,7 +48,7 @@ router.get("/login", (req, res) => {
 
 router.post("/login", authenticate());
 
-router.get("/posts/regist", (req, res) => {
+router.get("/posts/regist", authorize("readWrite"), (req, res) => {
   tokens.secret((error, secret) => {
     var token = tokens.create(secret);
     req.session._csrf = secret;
@@ -63,12 +57,12 @@ router.get("/posts/regist", (req, res) => {
   });
 });
 
-router.post("/posts/regist/input", (req, res) => {
+router.post("/posts/regist/input", authorize("readWrite"), (req, res) => {
   var original = createRegistData(req.body);
   res.render("./account/posts/regist-form.ejs", { original });
 });
 
-router.post("/posts/regist/confirm", (req, res) => {
+router.post("/posts/regist/confirm", authorize("readWrite"), (req, res) => {
   var original = createRegistData(req.body);
   var errors = validateRegistData(req.body);
   if (errors) {
@@ -78,7 +72,7 @@ router.post("/posts/regist/confirm", (req, res) => {
   res.render("./account/posts/regist-confirm.ejs", { original });
 });
 
-router.post("/posts/regist/execute", (req, res) => {
+router.post("/posts/regist/execute", authorize("readWrite"), (req, res) => {
   var secret = req.session._csrf;
   var token = req.cookies._csrf;
 
@@ -110,7 +104,7 @@ router.post("/posts/regist/execute", (req, res) => {
   });
 });
 
-router.get("/posts/regist/complete", (req, res) => {
+router.get("/posts/regist/complete", authorize("readWrite"), (req, res) => {
   res.render("./account/posts/regist-complete.ejs");
 });
 
